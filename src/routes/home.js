47cb9078b99f432986cae0b67e0a4cb004a74ad5
Nguyen Ngoc Duoc <duoc6694@gmail.com>
2023-07-23 21:25:33 +0700

    add function send mail

--- src/routes/home.js
+++ src/routes/home.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const homeController = require("../app/controllers/HomeController");
 
+router.post("/feedback", homeController.feedback);
 router.get("/", homeController.home);
 
 module.exports = router;
