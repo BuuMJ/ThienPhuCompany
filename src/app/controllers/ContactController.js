1a293c3ae3d11a70611e104e7cbbf980dba7c9ce
Nguyen Ngoc Duoc <duoc6694@gmail.com>
2023-07-25 22:08:11 +0700

    add function contact

--- src/app/controllers/ContactController.js
+++ src/app/controllers/ContactController.js
@@ -1,6 +1,46 @@
+const nodemailer = require("nodemailer");
+
 class ContactController {
   contact(req, res, next) {
     res.render("contact");
+  }
+
+  apicontact(req, res, next) {
+    const name = req.body.fullname;
+    const mail = req.body.email;
+    const phone = req.body.phone;
+    const address = req.body.address;
+    const content = req.body.content;
+    var transporter = nodemailer.createTransport({
+      service: "gmail",
+      auth: {
+        user: "duoc6694@gmail.com",
+        pass: "wdymtvgbhblstfbj",
+      },
+    });
+    const mailOptions = {
+      from: `"${name}" <${mail}>`,
+      to: "duoc6694@gmail.com", // list of receivers
+      subject: "Có một yêu cầu liên hệ với khác hàng mới", // Subject line
+      html: `
+      <p>Thông tin khách hàng cần liên hệ như sau:</p>
+      <ol>
+        <li>Tên khách hàng: ${name}</li>
+        <li>Email: ${mail}</li>
+        <li>Số điện thoại liên hệ: ${phone}</li>
+        <li>Địa chỉ liên hệ: ${address}</li>
+        <li>Nội dung liên hệ của khác hàng: ${content}</li>
+      </ol>
+    `,
+    };
+    transporter.sendMail(mailOptions, function (err, info) {
+      if (err) {
+        console.log(err);
+      } else {
+        console.log(info);
+        res.redirect("back");
+      }
+    });
   }
 }
 
--- src/routes/contact.js
+++ src/routes/contact.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const contactController = require("../app/controllers/ContactController");
 
+router.post("/api", contactController.apicontact);
 router.get("/", contactController.contact);
 
 module.exports = router;
