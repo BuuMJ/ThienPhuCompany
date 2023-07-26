11f8bbab1bbacabf4288659e1f3ca8d1752970db
Nguyen Ngoc Duoc <duoc6694@gmail.com>
2023-07-26 16:16:20 +0700

    add router products

--- src/app/controllers/ProductController.js
+++ src/app/controllers/ProductController.js
@@ -2,6 +2,26 @@ class ProductController {
   list(req, res, next) {
     res.render("product");
   }
+
+  pcb40(req, res, next) {
+    res.render("pcb40");
+  }
+
+  dadung(req, res, next) {
+    res.render("dadung");
+  }
+
+  pcb40ms(req, res, next) {
+    res.render("pcb40ms");
+  }
+
+  scg(req, res, next) {
+    res.render("scg");
+  }
+
+  longthanhpcb40(req, res, next) {
+    res.render("longthanhpcb40");
+  }
 }
 
 module.exports = new ProductController();
--- src/routes/product.js
+++ src/routes/product.js
@@ -2,6 +2,11 @@ const express = require("express");
 const router = express.Router();
 const productController = require("../app/controllers/ProductController");
 
+router.get("/pcb40", productController.pcb40);
+router.get("/dadung", productController.dadung);
+router.get("/pcb40ms", productController.pcb40ms);
+router.get("/longthanhpcb40", productController.longthanhpcb40);
+router.get("/scg", productController.scg);
 router.get("/", productController.list);
 
 module.exports = router;
