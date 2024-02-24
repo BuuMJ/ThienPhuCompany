a44833207d393e3c327e3ea5560c2861e10a6fd2
Kocoto <duoc6694@gmail.com>
2024-02-25 02:48:19 +0700

    add function authorize

---
+++ src/util/authorize.js
@@ -0,0 +1,30 @@
+const jwt = require("jsonwebtoken");
+const { company } = require("../app/controllers/CompanyController");
+const UserModel = require("../app/models/user")
+//check login
+function checkLogin(req, res, next) {
+  //check
+  try {
+    var token = req.cookies.token;
+    var idUser = jwt.verify(token, "PW");
+    UserModel.findOne({
+      _id: idUser,
+    }).then((data) => {
+      if (data) {
+        req.user = data;
+        // console.log(req.user);
+        return next();
+      } else {
+        res.render("login", {
+          title: "Login",
+          msg: "Please login.",
+        });
+      }
+    });
+  } catch (err) {
+    return res.render("login", {
+      title: "Login",
+      msg: "Please login.",
+    });
+  }
+}
