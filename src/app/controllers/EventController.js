b6fbd3ff4433edaab3f56b2667eff88546906190
BuuMJ <nxt03091999@gmail.com>
2024-03-01 18:05:14 +0700

    design event page

--- src/app/controllers/EventController.js
+++ src/app/controllers/EventController.js
@@ -3,17 +3,23 @@ const {mutipleMongooseToObject} = require("../../util/mongoose")
 
 class EventController {
   async event(req, res, next) {
-    const listEvent = await EventModel.find({ category: "event" });
-    const listNew = await EventModel.find({ category: "new" });
+    const latestNews = await EventModel.find().sort({ createdAt: -1 }).limit(1);
+    const threeLatestNews = await EventModel.find().sort({ createdAt: -1 }).skip(1).limit(3);
+    const remainingNews = await EventModel.find().sort({ createdAt: -1 }).skip(4);
+    const listEvent = await EventModel.find({ category: "event" }).sort({ createdAt: -1 });
+    const listNew = await EventModel.find({ category: "new" }).sort({ createdAt: -1 });
     console.log(
       listEvent +
         "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
     );
     console.log(mutipleMongooseToObject(listEvent));
     res.render("event", {
-      title: "Sự Kiện",
+      title: "Tin Tức Và Khuyến Mãi",
       listEvent: mutipleMongooseToObject(listEvent),
       listNew: mutipleMongooseToObject(listNew),
+      threeLatestNews: mutipleMongooseToObject(threeLatestNews),
+      latestNews: mutipleMongooseToObject(latestNews),
+      remainingNews: mutipleMongooseToObject(remainingNews),
     });
   }
 }
